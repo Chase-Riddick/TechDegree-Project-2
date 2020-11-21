7ec966c8e5b6cf32c898bb2e1e3b95287bd81a49
Chase-Riddick <70459435+Chase-Riddick@users.noreply.github.com>
2020-11-22 06:21:44 +0800

    Update application.py

--- application.py
+++ application.py
@@ -9,7 +9,9 @@ teams = constants.TEAMS
 
 def display_start_menu():
     """ \n\n\n Prints a message introducing the function/ purpose of application, as well as the input commands used to proceed with its use or        exit."""
-    print(" >>>>>> Baseball Stats Tool <<<<<\n")
+    
+    print("\n\n>>>>>> Baseball Stats Tool <<<<<")
+    print("-----------------------------------\n" )
     print(" Welcome to the Baseball Stats Tool")
     print("* This application separates a pool of players into three different baseball teams.")
     print("* The user can use the application to view a range of statistics on each of the teams.\n") 
@@ -33,7 +35,6 @@ def test_function(func):
         while value1 != accepted_value1 and value1 != accepted_value2:
             print(f"Sorry, '{value1}' isn't an accepted option. Please input 'C' or 'E' (Continue/ Exit)")
             value1 = user_input()
-    print(f"You entered: {value1}")
     return value1
 
 
@@ -42,13 +43,12 @@ def proceed_or_exit_app(value):
     if command_input == "E":
         exit_app()
     if command_input == "C":
-        print("Well, let's continue onward shall we? \n")
+        print("You've opted to'continue' - great choice!\n")
         
 
 def exit_app():
-    print("\n You have opted to 'Exit' this application.")
-    print("That's an unfortunate choice! But thanks for giving it a try at least.")
-    print("Have a great day, and we'll to see you around here again soon.")
+    print("\nYou've opted to 'Exit' this application.\n")
+    print("Thanks for taking the time to check out this application. \n Have a great day, and I'll hope to see you around here again soon.")
     sys.exit()
 
 
@@ -58,6 +58,7 @@ def convert_player_height_into_int(list):
         height = element['height'].split(" ")
         element['height'] = int(height[0])
     return list
+
 
 def convert_experience_into_bool(list):
     for element in list:
@@ -82,7 +83,7 @@ def separate_players_by_experience(list):
     return experienced_players_list, inexperienced_players_list
 
 
-def sort_players_into_team_by_experience(experienced_players_list, inexperienced_players_list, teams_list):
+def balance_teams(experienced_players_list, inexperienced_players_list, teams_list):
     """ """
     experienced_players = experienced_players_list 
     inexperienced_players = inexperienced_players_list 
@@ -113,10 +114,11 @@ def generate_teams(players_list, teams_list):
     playerslista = convert_player_height_into_int(playerslista)
     playerslista = convert_experience_into_bool(playerslista)
     experienced_players, inexperienced_players = separate_players_by_experience(playerslista)
-    new_teams = sort_players_into_team_by_experience(experienced_players, inexperienced_players, teams_list)
+    new_teams = balance_teams(experienced_players, inexperienced_players, teams_list)
 
     return new_teams
     
+
 def user_input_teams():
     """Collects users input """
     while True:
@@ -167,53 +169,62 @@ def display_team_statistics(teams_list_import, list_of_teams_and_players, user_s
     
     team_input = int(int(user_select_team_input) - 1)
     
+
     number_of_players = len(list_of_teams_and_players[team_input])
-    print(f"Total Players: {number_of_players}")
+    print(f"- Total Players: {number_of_players}")
+
 
     player_names = []
     for player in list_of_teams_and_players[team_input]:
         player_names.append(player['name'])
     player_names = (', ').join(player_names)
     # print((', ').join(player_names))
-    print(f"Player Names: {player_names}")
+    print(f"- Player Names: {player_names}")
   
+
     number_of_experienced_players = 0
     for player in list_of_teams_and_players[team_input]:
         if player['experience'] == True:
             number_of_experienced_players += 1
-    print(f"Experienced Players: {number_of_experienced_players}")
+    print(f"- Experienced Players: {number_of_experienced_players}")
   
+
     number_of_inexperienced_players = (number_of_players - number_of_experienced_players)
-    print(f"Inexperienced Players: {number_of_inexperienced_players}")
+    print(f"- Inexperienced Players: {number_of_inexperienced_players}")
   
+
     total_team_height = []
     for player in list_of_teams_and_players[team_input]:
         total_team_height.append(player['height'])
     average_height = (sum(total_team_height))/number_of_players
     average_height = round(average_height, 1)
-    print(f"Average Height: {average_height}")
+    print(f"- Average Height: {average_height}")
     # average_height = sum(total_team_height)/number_of_players
    
     
     guardian_names = []
-    print("Name of Guardians: ")
+    print("- Name of Guardians: ")
     for player in list_of_teams_and_players[team_input]:
-        x = player['guardians'].split("and")
+        x = player['guardians'].split(" and ")
         (', ').join(x)
         guardian_names.extend(x)
     
     print((', ').join(guardian_names))
 
 
-display_start_menu()
-user_command = test_function(user_input())
-proceed_or_exit_app(user_command)
-new_teams = generate_teams(playerslista, teams)
 
-while True: 
-    team_choice = teams_selection(teams)
-    display_team_statistics(teams, new_teams, team_choice)
-    print("Enter 'C' if you would like to continue using application.")
-    print("Enter 'E' if you would like to exit the application.")
+
+if __name__ == "__main__":
+
+    display_start_menu()
     user_command = test_function(user_input())
     proceed_or_exit_app(user_command)
+    new_teams = generate_teams(playerslista, teams)
+
+    while True: 
+        team_choice = teams_selection(teams)
+        display_team_statistics(teams, new_teams, team_choice)
+        print("\n\nEnter 'C' if you would like to continue using application to print the stats of another team.")
+        print("Enter 'E' if you would like to exit the application.")
+        user_command = test_function(user_input())
+        proceed_or_exit_app(user_command)
