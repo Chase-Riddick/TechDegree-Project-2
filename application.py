daf3f807f9716bd821023d6f50d7ed11b39eaa86
Chase-Riddick <70459435+Chase-Riddick@users.noreply.github.com>
2020-11-23 12:22:30 +0800

    Update application.py

--- application.py
+++ application.py
@@ -3,12 +3,12 @@ import sys
 import constants
 
 
-playerslista = constants.PLAYERS
-teams = constants.TEAMS
+players_list = constants.PLAYERS
+teams_list = constants.TEAMS
 
 
 def display_start_menu():
-    """ \n\n\n Prints a message introducing the function/ purpose of application, as well as the input commands used to proceed with its use or        exit."""
+    """ Prints a message introducing the function/ purpose of application; stipulates range of user commands."""
     
     print("\n\n>>>>>> Baseball Stats Tool <<<<<")
     print("-----------------------------------\n" )
@@ -22,79 +22,93 @@ def display_start_menu():
 
 def  user_input():
     """Collects users input """
+
     user_input = input(">>> ")
     return user_input
 
 
-def test_function(func):
-    value1 = func
-    accepted_value1 = "C" 
-    accepted_value2 = "E"
+def constrain_user_input(input_function):
+    """ Constrains user_input to C (continue) or E (exit)."""
+
+    input_value = input_function
+    accepted_values = ["C","E"]
+    accepted_values[0]
+    accepted_values[1]
     
-    if value1 != accepted_value1 or value1 != accepted_value2:
-        while value1 != accepted_value1 and value1 != accepted_value2:
-            print(f"Sorry, '{value1}' isn't an accepted option. Please input 'C' or 'E' (Continue/ Exit)")
-            value1 = user_input()
-    return value1
+    if input_value != accepted_values[0] or input_value != accepted_values[1]:
+        while input_value != accepted_values[0] and input_value != accepted_values[1]:
+            print(f"Sorry, '{input_value}' isn't an accepted option. Please input 'C' or 'E' (Continue/ Exit)")
+            input_value = user_input()
+
+    return input_value
 
 
-def proceed_or_exit_app(value):
-    command_input = value
-    if command_input == "E":
+def proceed_or_exit_app(user_command_input):
+    """ Runs user input, exiting if user inputted corresponding value."""
+
+    user_command = user_command_input
+    if user_command == "E":
         exit_app()
-    if command_input == "C":
-        print("You've opted to'continue' - great choice!\n")
+    print("You've opted to'continue' - great choice!\n")
         
 
 def exit_app():
+    """ Completes exit of application."""
+
     print("\nYou've opted to 'Exit' this application.\n")
     print("Thanks for taking the time to check out this application. \n Have a great day, and I'll hope to see you around here again soon.")
     sys.exit()
 
 
-def convert_player_height_into_int(list):
-    """ """
-    for element in list:
-        height = element['height'].split(" ")
-        element['height'] = int(height[0])
-    return list
+def convert_player_height_into_int(players_list_input):
+    """ Runs player list through to change height keyvalue from string into int."""
+
+    for player in players_list_input:
+        height = player['height'].split(" ")
+        player['height'] = int(height[0])
+    return players_list_input
 
 
-def convert_experience_into_bool(list):
-    for element in list:
-        if element['experience'].lower() == "yes":
-            element['experience'] = bool(1)
-        elif element['experience'].lower()  == "no":
-            element['experience'] = bool(0)
-    return list
+def convert_experience_into_bool(players_list_input):
+    """ Runs player list experience through to change experience keyvalue from string to Boolean value."""
+
+    for player in players_list_input:
+        if player['experience'].lower() == "yes":
+            player['experience'] = bool(1)
+        elif player['experience'].lower()  == "no":
+            player['experience'] = bool(0)
+    return players_list_input
 
 
-def separate_players_by_experience(list):
+def separate_players_by_experience(players_list_input):
+    """ Runs player list through to create two new player list based on whether on not player has experience."""
+
     experienced_players_list = []
     inexperienced_players_list = []
     
-    for element in list:
-        if element['experience'] == True:
-            experienced_players_list.append(element)
+    for player in players_list_input:
+        if player['experience'] == True:
+            experienced_players_list.append(player)
             
-        elif element['experience']  == False:
-            inexperienced_players_list.append(element)
+        elif player['experience'] == False:
+            inexperienced_players_list.append(player)
     
     return experienced_players_list, inexperienced_players_list
 
 
-def balance_teams(experienced_players_list, inexperienced_players_list, teams_list):
-    """ """
-    experienced_players = experienced_players_list 
-    inexperienced_players = inexperienced_players_list 
+def distribute_equally_players_by_experience(experienced_players_list_input, inexperienced_players_list_input, teams_list_input):
+    """Run through player list, creating two new teams based on experience."""
+    
+    experienced_players = experienced_players_list_input
+    inexperienced_players = inexperienced_players_list_input
     
     experienced_players = random.sample(experienced_players, len(experienced_players))
     inexperienced_players = random.sample(inexperienced_players, len(inexperienced_players))
 
-    experienced_players_per_team = int(len(experienced_players)/ len(teams_list))
-    inexperienced_players_per_team = len(inexperienced_players)/ len(teams_list)
+    experienced_players_per_team = int(len(experienced_players)/ len(teams_list_input))
+    inexperienced_players_per_team = len(inexperienced_players)/ len(teams_list_input)
     
-    teams_and_player_list = [None] * len(teams_list)
+    teams_and_player_list = [None] * len(teams_list_input)
 
     teams_and_player_list[0] = experienced_players[:experienced_players_per_team]
     teams_and_player_list[1] = experienced_players[experienced_players_per_team:int(experienced_players_per_team * 2)]
@@ -104,23 +118,24 @@ def balance_teams(experienced_players_list, inexperienced_players_list, teams_li
     teams_and_player_list[1].extend(inexperienced_players[experienced_players_per_team:int(experienced_players_per_team * 2)])
     teams_and_player_list[2].extend(inexperienced_players[int(experienced_players_per_team * 2):])
     
-    
     return teams_and_player_list
 
 
-def generate_teams(players_list, teams_list):
-    teamlista = teams_list
-    playerslista = players_list
-    playerslista = convert_player_height_into_int(playerslista)
-    playerslista = convert_experience_into_bool(playerslista)
-    experienced_players, inexperienced_players = separate_players_by_experience(playerslista)
-    new_teams = balance_teams(experienced_players, inexperienced_players, teams_list)
+def generate_teams(players_list_input, teams_list_input):
+    """ This function combines the following functions to process the players list, and generate completed teams lists from it."""
 
-    return new_teams
+    function_players_list = players_list_input
+    function_players_list = convert_player_height_into_int(function_players_list)
+    function_players_list = convert_experience_into_bool(function_players_list)
+    experienced_players, inexperienced_players = separate_players_by_experience(function_players_list)
+    newly_generated_teams = distribute_equally_players_by_experience(experienced_players, inexperienced_players, teams_list)
+
+    return newly_generated_teams
     
 
 def user_input_teams():
     """Collects users input """
+
     while True:
         try:
             user_input = int(input(">>> "))
@@ -132,6 +147,8 @@ def user_input_teams():
 
 
 def display_teamselection_options(teams_list_import):
+    """ Takes teams list, and prompts user to choose what team to print stats on."""
+
     teams_list = teams_list_import
     top_of_teams_range = len(teams_list)
     print("\nSo, then which of the baseball teams below would you like to print stats for?")
@@ -142,6 +159,8 @@ def display_teamselection_options(teams_list_import):
         
 
 def get_valid_input(teams_list_import):
+    """ This constrains user input to choosing a value corresponding to one of the teams."""
+
     value = int(user_input_teams())
     team_max_range = len(teams_list_import)
     
@@ -155,7 +174,8 @@ def get_valid_input(teams_list_import):
     return value
     
     
-def teams_selection(teams_list_import):
+def teams_selection_function(teams_list_import):
+    """ This function combines the previous few together to bring together process for user choosing a team."""
     display_teamselection_options(teams_list_import)
     team_selection = get_valid_input(teams_list_import)
     return team_selection
@@ -212,19 +232,29 @@ def display_team_statistics(teams_list_import, list_of_teams_and_players, user_s
     print((', ').join(guardian_names))
 
 
-
+""" 
+    The rundown:
+    Display start menu.
+    Does the user want to continue to use app or exit?
+    Process command accordingly.
+    If user wishes to continue...
+    First generate the teams by processing player data, and sifting them according to experience.
+    Then give the user a menu of options have them choose a team to print stats on.
+    Print the stats for the team they selected.
+    Take user input to allow them them to exit the app or return to the team selection menu to print stats from another team. 
+."""
 
 if __name__ == "__main__":
 
     display_start_menu()
-    user_command = test_function(user_input())
+    user_command = constrain_user_input(user_input())
     proceed_or_exit_app(user_command)
-    new_teams = generate_teams(playerslista, teams)
+    filled_teams_list = generate_teams(players_list, teams_list)
 
     while True: 
-        team_choice = teams_selection(teams)
-        display_team_statistics(teams, new_teams, team_choice)
+        team_selection = teams_selection_function(teams_list)
+        display_team_statistics(teams_list, filled_teams_list, team_selection)
         print("\n\nEnter 'C' if you would like to continue using application to print the stats of another team.")
         print("Enter 'E' if you would like to exit the application.")
-        user_command = test_function(user_input())
+        user_command = constrain_user_input(user_input())
         proceed_or_exit_app(user_command)
